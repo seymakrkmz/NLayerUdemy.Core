2854cbdccf9517f8e1666e85ddf47dad924c1365
10024287790 <korkmazseyma195@gmail.com>
2022-03-28 15:03:20 +0300

    Add project files.

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity
+    {
+        //abstract yapmamızın sebebi nesne örneği alınmasın
+        //abstractlar ve interfaceler new anahtar sözcüğüyle tanımlanamaz.
+
+
+
+        public int id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
