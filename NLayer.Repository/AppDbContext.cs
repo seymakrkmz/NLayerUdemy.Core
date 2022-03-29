44f23c491d3a779b63adc15d0f59b10c7f0ee320
10024287790 <korkmazseyma195@gmail.com>
2022-03-29 14:50:30 +0300

    AppDbContext

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)   
+        {
+          //  var p =new Product() { ProductFeature=new ProductFeature() };   
+
+        }
+        public DbSet <Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
