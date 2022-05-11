a1ba7f238bb51daf66698ed9d8a496bc1f2d8c7d
10024287790 <korkmazseyma195@gmail.com>
2022-05-11 10:45:12 +0300

    DTO

---
+++ NLayer.Core/DTOs/BaseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public abstract class BaseDto
+    {
+
+        public int id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
